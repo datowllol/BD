56b944208a241ae31b9cc55ce07dc56b4f32020b
datowllol <datowl678@gmail.com>
2020-12-21 13:37:18 +0200

    Hello world

---
+++ src/main/java/com/bd/pubBooking/PubBookingApplication.java
@@ -0,0 +1,13 @@
+package com.bd.pubBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd/pubBooking/PubBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd.pubBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
